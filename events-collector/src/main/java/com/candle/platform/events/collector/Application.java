753accf91cf03acd6ef22e58fb3e92077d466e0d
Ankur Gurha <agurha@AGurha.local>
2016-06-16 21:22:05 +0100

    Initial commit

---
+++ events-collector/src/main/java/com/candle/platform/events/collector/Application.java
@@ -0,0 +1,21 @@
+package com.candle.platform.events.collector;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by agurha on 16/06/2016.
+ */
+@SpringBootApplication
+public class Application {
+
+    private static final Class<Application> appClass = Application.class;
+
+    private static final Logger log = LoggerFactory.getLogger(appClass);
+
+    public static void main(String[] args) {
+        SpringApplication.run(appClass, args);
+    }
+}
